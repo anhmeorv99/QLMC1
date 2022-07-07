a6c37295112a4fd99fe9f0e5ad968912b90efe6a
hientrantt <hientrantt211@gmail.com>
2022-07-07 12:45:04 +0700

    show data /minhchungcsgd

--- resources/views/layouts/dash.blade.php
+++ resources/views/layouts/dash.blade.php
@@ -18,7 +18,9 @@
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <img src="image/avatar.png" class="user-image" alt="User">
-              <span class="hidden-xs">{{ Auth::user()->name }}</span>
+              {{--<span class="hidden-xs">{{ Auth::user()->name }}</span> --}} 
+          <!-- comment hear -->
+
             </a>
             <ul class="dropdown-menu" style="background-color: #F1F1F1; width:50%;" >
               <li class="user-footer" style="background-color: #F1F1F1; width: 60%;margin-left: 50px;">
@@ -44,7 +46,8 @@
           <img src="image/avatar.png" class="img-circle" alt="User">
         </div>
         <div class="pull-left info">
-          <p>{{ Auth::user()->name }}</p>
+          {{-- <p>{{ Auth::user()->name }}</p> --}} 
+          <!-- comment hear -->
           <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
         </div>
       </div>
